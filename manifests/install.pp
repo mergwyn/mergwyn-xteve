eb97e1865ee10654e87ad65a69b0a6f33ea56529
mergwyn <gary@theclarkhome.com>
2018-12-19 17:53:53 +0000

    First pass at adding code based on jackett

--- manifests/install.pp
+++ manifests/install.pp
@@ -5,20 +5,9 @@
 # @example
 #   include xteve::install
 class xteve::install {
-  # Make sure version has is a string with at least 1 char
-  unless $::xteve_version =~ String[1] {
-    fail ("xteve_version is '${::xteve_version}'")
-  }
-  $package_name    = 'Jackett.Binaries.Mono'
-  $package_version = $::xteve_version
-  $install_path    = $::xteve::install_path
-  $extract_dir     = "${install_path}/Jackett-${package_version}"
-  $creates         = "${extract_dir}/Jackett"
-  $link            = "${install_path}/Jackett"
-  $repository_url  = 'https://github.com/Jackett/Jackett/releases/download/'
-  $package_source  = "${repository_url}/${package_version}/${package_name}.tar.gz"
-  $archive_name    = "${package_name}-${package_version}.tar.gz"
-  $archive_path    = "${install_path}/${archive_name}"
+  $install_path = $::xteve::install_path
+  $extract_dir  = "${install_path}/xteve"
+  $download_url = 'https://xteve.de/download/xteve_linux'
 
   if $xteve::package_manage {
     file { $extract_dir:
@@ -27,32 +16,16 @@ class xteve::install {
       group  => $::xteve::group,
     }
 
-    archive { $archive_name:
-      path         => $archive_path,
-      source       => $package_source,
-      extract      => true,
-      extract_path => $extract_dir,
-      creates      => $creates,
-      cleanup      => true,
-      user         => $::xteve::user,
-      group        => $::xteve::group,
-      #TODO reference to mono
-      #require      => Class['mono'],
-      notify       => Service['xteve.service'],
+    wget::fetch { 'xteve binary':
+      source      => "${download_url}",
+      destination => "${extract_dir}/",
+      cache_dir   => '/var/cache/wget',
+      timeout     => 15,
+      verbose     => true,
+      execuser    => $::xteve::user,
+      group       => $::xteve::group,
     }
-    file { $link:
-      ensure    => 'link',
-      target    => $creates,
-      subscribe => Archive[$archive_name],
-    }
-    exec {'xteve_tidy':
-      cwd         => $install_path,
-      path        => '/usr/sbin:/usr/bin:/sbin:/bin:',
-      command     => "ls -d ${link}-* | head -n -${xteve::keep} | xargs rm -rf",
-      #onlyif      => "test $(ls -d ${link}-* | wc -l) -gt ${xteve::keep}",
-      refreshonly => true,
-      subscribe   => Archive[$archive_name],
-    }
+
   }
 }
 # vim: number tabstop=8 expandtab shiftwidth=2 softtabstop=2
